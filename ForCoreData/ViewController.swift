6029a1cb9642ad3af9fa13997f092d54557973f6
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2020-06-07 07:57:53 -0400

    Initial Commit

---
+++ ForCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForCoreData
+//
+//  Created by Zhaoyang Li on 6/7/20.
+//  Copyright © 2020 Zhaoyang Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
